39bc000009854fe8e27326ae68e3144affca1c67
tom gauthier <tomgauthier0@gmail.com>
2021-09-20 10:30:46 +0200

    implementing the different carousels

--- script.js
+++ script.js
@@ -1,45 +1,167 @@
-// do promise validate promise
-
-// code w Axios
-// import axios from '.node_modules/axios.js';
-
 const root = "http://localhost:8000/api/v1/titles/"
 
-var dict = {
-	sort_by: "-imdb_score",
-	year: 2020
+const body = document.body
 
+
+
+//CAROUSEL JS
+
+const prev = document.querySelector('.left-arrow');
+const next = document.querySelector('.right-arrow');
+
+// const track = document.querySelector('.track');
+
+let index = 0;
+
+
+const slideNext = (id, index) => {
+	index++;
+	prev.classList.add('show');
+	tracks = document.getElementsByClassName('track')
+	track = tracks[id]
+	console.log(track)
+	track.style.transform = `translateX(-${index * 200}px)`;
+	console.log(track.offsetWidth)
+	console.log(index * 200)
+	if (track.offsetWidth - (index * 200) < 1200) {
+		next.classList.add('hide');
+	}
 }
 
 
-const createSection = (row, number) => {
-	// create a section with id number +1
-	let section = document.createElement("section")
-	section.id = `section${number+1}`
-	// create the arriw left
+const slidePrev = () => {
+	index--;
+	track = section
+	next.classList.remove('hide');
+	if (index === 0) {
+		prev.classList.remove('show');
+	}
+	track.style.transform = `translateX(-${index * 200}px)`;
+}
+
+
+// API JS
+
+
+
+const openModal = (id) => {
+	// get modal by id
+	selectedModal = document.getElementById(`myModal${id}`);
+	console.log(selectedModal)
+	selectedModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+
+const closeModal = (id) => {
+	selectedModal = document.getElementById(`myModal${id}`);
+	console.log(selectedModal)
+	selectedModal.style.display = "none";
+
+}
+
+const createModal = (item) => {
+	// create the modal <div id="myModal" class="modal">
+	let myModal = document.createElement("div")
+	myModal.classList.add("modal")
+	myModal.id = `myModal${item.id}`
+	// <div class="modal-content">
+	let modalContent = document.createElement("div")
+	modalContent.classList.add("modal-content")
+	// <span class="close">&times;</span>
+	let closeButton = document.createElement("span")
+	closeButton.classList.add("close")
+	closeButton.innerHTML = "&times;"
+	let image = document.createElement("img")
+	image.src = `${item.image_url}`
+
+	let textArea = document.createElement("div")
+	textArea.classList.add("textArea")
+	let title = document.createElement("h1")
+	title.textContent = `${item.title}`
+	console.log(item.title)
+	let genres = document.createElement("p")
+	genres.textContent = `${item.genres}`
+	let year = document.createElement("p")
+	year.textContent = `${item.year}`
+	// let rating = document.createElement("p")
+	// title.textContent = `${item.rating}`
+	let imdbScore = document.createElement("p")
+	imdbScore.textContent = `${item.imdb_score}`
+	let directors = document.createElement("p")
+	directors.textContent = `${item.directors}`
+	let actors = document.createElement("p")
+	actors.textContent = `${item.actors}`
+	// let duration = document.createElement("p")
+	// title.textContent = `${item.duration}`
+	// let country = document.createElement("p")
+	// title.textContent = `${item.country}`
+	// let boxOffice = document.createElement("p")
+	// boxOffice.textContent = `${item.boxOffice}`
+	// let summary = document.createElement("p")
+	// boxOffice.textContent = `${item.summary}`
+	textArea.appendChild(title)
+	textArea.appendChild(genres)
+	textArea.appendChild(year)
+	// textArea.appendChild(rating)
+	textArea.appendChild(imdbScore)
+	textArea.appendChild(directors)
+	textArea.appendChild(actors)
+	// textArea.appendChild(duration)
+	// textArea.appendChild(country)
+	// textArea.appendChild(boxOffice)
+	// textArea.appendChild(summary)
+	modalContent.appendChild(closeButton)
+	modalContent.appendChild(image)
+	modalContent.appendChild(textArea)
+	myModal.appendChild(modalContent)
+	itemId = item.id
+	console.log("itemId: " + itemId)
+	myModal.setAttribute("onclick", "closeModal(" + itemId.toString() + ")")
+	console.log(myModal)
+	return myModal
+}
+
+const createSection = (row, id) => {
+	let carouselContainer = document.getElementById(id)
+	console.log('carouselContainer: ' + carouselContainer)
+	let carouselInner = carouselContainer.getElementsByClassName('carousel-inner')[0]
+	let track = carouselInner.getElementsByClassName('track')[0]
+	track.id = id
+	// create the arrow left
 	let leftArrow = document.createElement("a")
-	leftArrow.href=`#section${number}`
 	leftArrow.classList.add("arrow__btn", "left-arrow")
 	leftArrow.textContent = "‹"
-	section.appendChild(leftArrow)
+	leftArrow.addEventListener("click", slidePrev)
+	carouselContainer.appendChild(leftArrow)
 	// for each item in row, create the item
 	row.forEach(item => {
+		console.log(item)
 		let divItem = document.createElement("div")
 		divItem.classList.add("item")
+		console.log("item.id " + item.id)
+		itemId = item.id
+		divItem.setAttribute("onclick", "openModal(" + itemId.toString() + ")")
 		let image = document.createElement("img")
 		image.src = `${item.image_url}`
+		let modal = createModal(item)
 		divItem.appendChild(image)
+		body.appendChild(modal)
 		// append each item to the section
-		section.appendChild(divItem)
+		track.appendChild(divItem)
 	})
+	console.log(track)
 	// create and append the arrow right
 	let rightArrow = document.createElement("a")
-	rightArrow.href=`#section${number+2}`
 	rightArrow.classList.add("arrow__btn", "right-arrow")
 	rightArrow.textContent = "›"
-	section.appendChild(rightArrow)
-	// return the section
-	return section
+	console.log(index)
+	console.log(typeof(id))
+	console.log(typeof(index))
+	console.log("slideNext("+ id.toString()+","+index+")")
+	let param = "slideNext("+ id.toString()+","+index+")"
+	rightArrow.addEventListener("click", param)
+	carouselContainer.appendChild(rightArrow)
 }
 
 const splitMovies = (allPages) => {
@@ -59,7 +181,7 @@ async function loadCarousel(id, dict) {
 		params: dict
 	})
 	console.log(response)
-	const numPages = Math.ceil(response.data.count / 5)
+	const numPages = 2; // Math.ceil(response.data.count / 5)
 	console.log(numPages);
 	// now get an array of all the requests
 	requests = []
@@ -74,75 +196,27 @@ async function loadCarousel(id, dict) {
 	allPages = await axios.all(requests)
 	console.log(allPages)
 	table = splitMovies(allPages)
-	const carousel = document.getElementById(id)
-	table.forEach( (row, index)  => {
-		// slide should return an html object
-		slide = createSection(row, index)
-		carousel.appendChild(slide)
-	})
+	createSection(table[0], id)
 }
 
 
 
-loadCarousel("bestMovies", {
-	sort_by: "-imdb_score",
-	year: 2020
-})
+// loadCarousel("bestMovies", {
+// 	sort_by: "-imdb_score",
+// 	year: 2020
+// })
 
 loadCarousel("worstOfCage", {
-	actor : "Nicolas Cage",
+	actor: "Nicolas Cage",
 	sort_by: "imdb_score"
 })
 
-loadCarousel("2010", {
-	sort_by: "-imdb_score",
-	year: 2010
-})
+// loadCarousel("2010", {
+// 	sort_by: "-imdb_score",
+// 	year: 2010
+// })
 
 
-
-
-// const createSection = (page, sectionName) => {
-// 	console.log(`section: ${sectionName}`)
-// 	var section = document.getElementById(sectionName)
-// 	console.log(section)
-// 	if (section == null) {
-// 		section = document.createElement("section")
-// 	} else {
-// 		section = document.getElementById(sectionName)
-// 		section.innerHTML = ""
-// 		console.log("else")
-// 	}
-// 	section.id = sectionName
-// 	section.classList.add("section")
-// 	const wrap = document.getElementsByClassName("wrapper")[0]
-// 	wrap.appendChild(section)
-// 	console.log(section)
-
-// 	const items = page.results
-// 	const arrowLeft = document.createElement("a")
-// 	arrowLeft.onclick = function() {
-// 		getPage(page.previous, {}, sectionName)
-// 	}
-// 	arrowLeft.textContent = "‹"
-// 	arrowLeft.href = `#section5`
-// 	arrowLeft.classList.add("arrow__btn", "left-arrow")
-// 	const arrowRight = document.createElement("a")
-// 	// arrowRight.href = `#` // try calling function getPage with the next page
-// 	arrowRight.onclick = function() {
-// 		getPage(page.next, {}, sectionName)
-// 	}
-// 	arrowRight.classList.add("arrow__btn", "right-arrow")
-// 	arrowRight.textContent = "›"
-// 	section.appendChild(arrowLeft)
-// 	section.appendChild(arrowRight)
-
-// 	console.log(items)
-// 	items.forEach(function(item) {
-// 		movie = createItem(item)
-// 		section.appendChild(movie)
-// 	})
-// };
 
 // pass the section as an argument instead of fetching it back each time
 const createItem = (movie) => {
